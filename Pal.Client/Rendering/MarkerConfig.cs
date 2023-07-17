d88b8bc732896cdde792ee177bde7f09794860b5
NiGuangOwO <m2589895500@gmail.com>
2023-07-17 15:18:22 +0800

    Update MarkerConfig.cs

--- Pal.Client/Rendering/MarkerConfig.cs
+++ Pal.Client/Rendering/MarkerConfig.cs
@@ -10,7 +10,7 @@ namespace Pal.Client.Rendering
         private static readonly Dictionary<MemoryLocation.EType, MarkerConfig> MarkerConfigs = new()
         {
             { MemoryLocation.EType.Trap, new MarkerConfig { Radius = 1.7f } },
-            { MemoryLocation.EType.Hoard, new MarkerConfig { Radius = 1.7f, OffsetY = -0.03f } },
+            { MemoryLocation.EType.Hoard, new MarkerConfig { Radius = 1.7f, OffsetY = 0.1f } },
             { MemoryLocation.EType.SilverCoffer, new MarkerConfig { Radius = 1f } },
             { MemoryLocation.EType.GoldCoffer, new MarkerConfig { Radius = 1f } },
         };
